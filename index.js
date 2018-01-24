8d5fe821aa78f7d70fd589f0b336ea2aa05c17d6
Stef Siekman <s.siekman@gmail.com>
2018-01-24 21:06:11 +0100

    Create inital server

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.json({ message: "Hello, world!" })
+})
+
+app.listen(PORT, () => console.log(`Server running on ${ PORT }`))
