8edb90c0cb5c5777bb8da0d89ddfde99b74b5ef0
Stef Siekman <s.siekman@gmail.com>
2018-01-24 22:59:14 +0100

    Improve the AI name validator

--- util/validator.js
+++ util/validator.js
@@ -1,7 +1,9 @@
 module.exports = {
 
     validAIName: (name) => {
-        return name.match(/^[a-z]{1}[a-z0-9-]{2,30}[a-z0-9]{1}$/)
+        return name != undefined &&
+                typeof name == typeof "" &&
+                name.match(/^[a-z]{1}[a-z0-9-]{2,30}[a-z0-9]{1}$/)
     }
 
 }
