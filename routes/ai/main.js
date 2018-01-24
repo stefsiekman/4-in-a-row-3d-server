2f1fe7e34c87023ebfab361715458132bfed9d5a
Stef Siekman <s.siekman@gmail.com>
2018-01-24 22:11:25 +0100

    Add listing of AIs

--- routes/ai/main.js
+++ routes/ai/main.js
@@ -4,18 +4,22 @@ const router = express.Router()
 
 router.get("/", (req, res) => {
     pg.connect(process.env.DATABASE_URL, (err, client, done) => {
-        client.query("SELECT * FROM ais;", (err, result) => {
-            done();
-            if (err) {
-                console.error(err)
-                res.json({
-                    error_code: 1,
-                    error_message: "Database connection error"
-                })
-            } else {
-                res.json(result.rows)
-            }
-        })
+        if (err) {
+            console.error(err)
+        } else {
+            client.query("SELECT * FROM ais WHERE id=1;", (err, result) => {
+                done();
+                if (err) {
+                    console.error(err)
+                    res.json({
+                        error_code: 1,
+                        error_message: "Database connection error"
+                    })
+                } else {
+                    res.json(result.rows)
+                }
+            })
+        }
     })
 })
 
