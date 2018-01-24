4d7b5cce1c05d86505df8df15f9a90867c49f3d6
Stef Siekman <s.siekman@gmail.com>
2018-01-24 21:17:14 +0100

    Add get /ai route

--- index.js
+++ index.js
@@ -7,4 +7,8 @@ app.get("/", (req, res) => {
     res.json({ message: "Hello, world!" })
 })
 
+app.get("/ai", (req, res) => {
+    res.json({ error_message: "No AIs configured yet" })
+})
+
 app.listen(PORT, () => console.log(`Server running on ${ PORT }`))
