e37b7f8ea3dbf24b4bb5ec2942676894776c5871
Stef Siekman <s.siekman@gmail.com>
2018-01-29 17:26:36 +0100

    Fix joining game on new version of PG

--- util/join-game.js
+++ util/join-game.js
@@ -42,41 +42,31 @@ function joinNewGame(res, aiId, callback) {
 // Method to get next possible game to join
 module.exports = (res, aiId, callback) => {
 
-    pool.connect(process.env.DATABASE_URL, (err, done) => {
-        // Check whether to send an error message
+    // Get all the open games (should be 1 or 0)
+    var sql = "SELECT * FROM games WHERE status=1 AND ai_b IS NULL "
+            + "AND ai_a != $1;"
+    var values = [ aiId ]
+    pool.query(sql, values, (err, result) => {
+        // Check for errors
         if (err) {
-            throw err
             error.respondJson(res, 1)
             done()
             return
         }
 
-        // Get all the open games (should be 1 or 0)
-        var sql = "SELECT * FROM games WHERE status=1 AND ai_b IS NULL "
-                + "AND ai_a != $1;"
-        var values = [ aiId ]
-        pool.query(sql, values, (err, result) => {
-            // Check for errors
-            if (err) {
-                error.respondJson(res, 1)
-                done()
-                return
-            }
+        // Callback for when a game is joined
+        var gameJoinedCallback = (game) => {
+            done()
+            callback(game)
+        }
 
-            // Callback for when a game is joined
-            var gameJoinedCallback = (game) => {
-                done()
-                callback(game)
-            }
-
-            // If something is returned, that game can be joined
-            if (result.rows[0]) {
-                joinExistingGame(res, aiId, result.rows[0],
-                    gameJoinedCallback)
-            } else {
-                joinNewGame(res, aiId, gameJoinedCallback)
-            }
-        })
+        // If something is returned, that game can be joined
+        if (result.rows[0]) {
+            joinExistingGame(res, aiId, result.rows[0],
+                gameJoinedCallback)
+        } else {
+            joinNewGame(res, aiId, gameJoinedCallback)
+        }
     })
 
 }
